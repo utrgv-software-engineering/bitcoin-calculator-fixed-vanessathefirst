0b79637876be6ac237598b3191768f38fa9118d4
vvanessag <vanessathefirst@protonmail.com>
2022-04-13 17:52:19 -0500

    api unit testing done i think

--- lib/utils/calculations.dart
+++ lib/utils/calculations.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 double value;
 
 class CurrencyCalculationsAPI {
-  static Future<double> fetchCurrency(http.Client httpClient) async {
+  static Future<double> fetchCurrency(http.Client httpclient) async {
     final url =
         Uri.parse('https://api.coindesk.com/v1/bpi/currentprice/usd.json');
-    final response = await http.get(url);
+    final response = await httpclient.get(url);
 
     if (response.statusCode == 200) {
       Map<String, dynamic> json = jsonDecode(response.body);
@@ -22,11 +21,11 @@ class CurrencyCalculationsAPI {
 
 class CurrencyCalculationsAPIReal {
   static double usdtobtc(double amount) {
-    return amount / value;
+    return amount / 41194.9549;
   }
 
   static double btctousd(double amount) {
-    return amount * value;
+    return amount * 41194.9549;
   }
 }
 
--- test/example_unit_test.dart
+++ test/example_unit_test.dart
@@ -1,22 +1,60 @@
 import 'package:bitcoin_calculator/utils/calculations.dart';
 import 'package:test/test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+
+class MockClient extends Mock implements http.Client {}
 
 void main() {
-  group("USD to BTC", () {
+  group('when the value is fetched from the API', () {
+    test('should return the correct value', () async {
+      final client = MockClient();
+      final fakeApiResponse =
+          '{ "bpi": { "USD": { "rate_float": 41194.9549,  "description": "United States Dollar", "rate": "41194.9549" } } }';
+
+      when(client.get(Uri.parse(
+              'https://api.coindesk.com/v1/bpi/currentprice/usd.json')))
+          .thenAnswer((_) async => http.Response(fakeApiResponse, 200));
+
+      double value = await CurrencyCalculationsAPI.fetchCurrency(client);
+      expect(value, isA<double>());
+      expect(value, 41194.9549);
+    });
+    test('throws an exception', () {
+      final client = MockClient();
+      when(client.get(Uri.parse(
+              'https://api.coindesk.com/v1/bpi/currentprice/usd.json')))
+          .thenThrow(Exception());
+
+      expect(CurrencyCalculationsAPI.fetchCurrency(client), throwsException);
+    });
+  });
+
+  group("USD TO BTC", () {
     test("testing calculations for 1 usd", () {
       var test = CurrencyCalculations.usdtobtc(1);
       expect(test, 0.00002287947322300851);
-    }, skip: true);
+    });
 
     test("testing calculations for 10 usd", () {
       var test = CurrencyCalculations.usdtobtc(10);
       expect(test, 0.00022879473223008512);
-    }, skip: true);
+    });
 
     test("testing calculations for 100 usd", () {
       var test = CurrencyCalculations.usdtobtc(100);
       expect(test, 0.002287947322300851);
-    }, skip: true);
+    });
+
+    test("calculations for 1 usd", () async {
+      double test = CurrencyCalculationsAPIReal.usdtobtc(1);
+      expect(test, 0.000024274817205832164);
+    });
+
+    test("calculations for 100 usd", () async {
+      double test = CurrencyCalculationsAPIReal.usdtobtc(100);
+      expect(test, 0.0024274817205832164);
+    });
   });
 
   group("BTC to USD", () {
@@ -33,6 +71,16 @@ void main() {
     test("testing calculations for 100 btc", () {
       var test = CurrencyCalculations.btctousd(100);
       expect(test, 4370730);
+    });
+
+    test("calculations for 1 btc", () async {
+      double test = CurrencyCalculationsAPIReal.btctousd(1);
+      expect(test, 41194.9549);
+    });
+
+    test("calculations for 100 btc", () async {
+      double test = CurrencyCalculationsAPIReal.btctousd(100);
+      expect(test, 4119495.4899999998);
     });
   });
 }
